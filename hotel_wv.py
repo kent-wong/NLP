21fecf990430090d94c9ed4c712bd58cef990734
kent-wong <w02682@gmail.com>
2018-09-20 22:02:31 +0800

    modify

--- hotel_wv.py
+++ hotel_wv.py
@@ -1,4 +1,5 @@
 import gensim
+from gensim.models import Word2Vec
 import logging
 import numpy as np
 import pandas as pd
@@ -17,15 +18,20 @@ df = pd.read_csv(fname, sep=',',
 print()
 
 print('\n=====================')
+del df['hotel_name']
 del df['user_name']
 del df['time']
 del df['scrape_time']
-df.hotel_name = 'hotel_001'
-df.score[df.score < 4.0] = 0  # notice: do NOT interchange this and below lines!
+#df.hotel_name = 'hotel_001'
+
+# get rid of 'NaN'
+#df.score[df.score.isnull()] = 0
+df.dropna(inplace=True)
+
+# NOTICE: do NOT interchange these two lines
+df.score[df.score < 4.0] = 0
 df.score[df.score > 0] = 1
-#df.score.apply(lambda x : int(x))
-#print(df.dtypes)
-#print(df.head(100))
+
 
 # read in stop words list
 stop_words_fname = 'stopwords.txt'
@@ -38,42 +44,14 @@ except FileNotFoundError:
     print("file name '{}' NOT found!".format(stop_words_fname))
 
 
-# cut words and filter out stopwords
-comment_sents = []
-print('\n=====================')
-for text in df.comment:
-    #print("text({}): ".format(type(text)), text)
-    if not isinstance(text, str):
-        continue
-    word_list = list(jieba.cut(text))
+model = Word2Vec.load('models/word2vec.model')
+
+def to_comment_vector(comment):
+    word_list = list(jieba.cut(comment))
     filtered = [w for w in word_list if w not in stop_words]
-    comment_sents.append(filtered)
+    a = np.array([model.wv[w] for w in filtered if w in model])
+    return a.mean(axis=0)
 
-# debug info
-#print(comment_sents[:10])
-print('total comment sentences:', len(comment_sents))
-
-all_words = []
-for sentence in comment_sents:
-    all_words += sentence
-
-theDict = set(all_words)
-#print(theDict)
-print(len(theDict))
-
-n_features = 100
-n_cores = 4
-window_size = 10
-downsampling = 1e-3
-
-print('Training model ...')
-model = gensim.models.Word2Vec(comment_sents, size=n_features, 
-                                window=window_size, sample=downsampling,
-                                min_count=5, workers=n_cores)
-
-print('training loss:', model.get_latest_training_loss())
-w = all_words[13]
-#print(model[w])
-print('------------------------')
-print('word is:', w)
-print(model.most_similar(w))
+df['vec'] = df.comment.apply(to_comment_vector)
+df.dropna(inplace=True)
+print(df)
